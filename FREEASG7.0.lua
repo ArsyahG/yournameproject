3742ef1b54c7728008e841151ff60f6f37263f12
ArsyahG <42270735+ArsyahG@users.noreply.github.com>
2018-08-11 16:31:19 +0700

    Add files via upload

---
+++ FREEASG7.0.lua
@@ -0,0 +1,355 @@
+gg.toast("loading....")
+gg.makeRequest("https://raw.githubusercontent.com/ArsyahG/ASG/master/FreeArsyahG0.7.0.lua")
+gg.toast(os.date([[
+Today: %Y/%m/%d Time: %H:%M:%S
+www.youtube.com/c/ArsyahG]]))
+gg.alert("@OrangBias4")
+gg.toast("www.youtube.com/c/ArsyahG")
+HOME = 1
+function HOME()
+  HM = gg.choice({
+    "\226\158\168\239\184\143 BYPASS ",
+    "🗡 WallHack {In Game} ",
+    "🗡 Red support HDR {In Game} ",
+    "🗡 Gren {In Game}",
+    "🇰 Speed Hack ",
+    "🇪 Super AimBot {In Game}",
+    "🇨 No Recoil {Lobby} ",
+    "🇪 No Grass ",
+	  "🇼 black sky ",
+	  "🇦 Lift Aim ",
+	  "🗡 Antenna ",
+    "🗡 Open Sit Scope",
+    "🗡 Off Sit Scope ",
+    "🗡 New Magic Bullet {Lobby} ",
+    "🗡 White All device(lobby)",
+    "🗡 Antenna Run",
+    "💳 Donation ",
+   	"✉ My Contact ",
+   	"❎ EXIT ❎"
+	}, nil, [[
+🗡Free Script PUBG mobile 0.7 🇮🇩
+Youtube: Arsyah G
+Telegram: @OrangBias4
+Instagram: @arsyah_g]])
+if HM == 1 then
+    BYPASS()
+  end
+  if HM == 2 then
+    M1()
+  end
+  if HM == 3 then
+    M2()
+  end
+  if HM == 4 then
+    M3()
+  end
+  if HM == 5 then
+    M4()
+  end
+  if HM == 6 then
+    M5()
+  end
+  if HM == 7 then
+    M6()
+  end
+  if HM == 8 then
+    M7()
+  end
+  if HM == 9 then
+    M8()
+  end
+  if HM == 10 then
+    M9()
+  end
+  if HM == 11 then
+    M10()
+  end
+  if HM == 12 then
+    M11()
+  end
+  if HM == 13 then
+    M12()
+  end
+   if HM == 14 then
+    M13()
+  end
+   if HM == 15 then
+    M14()
+  end
+   if HM == 16 then
+    M15()
+  end
+ if HM == 17 then
+    MDN()
+  end
+  if HM == 18 then
+    MC()
+  end
+  if HM == 19 then
+    EXIT()
+  end
+  HOMEDM = -1
+end
+function BYPASS()
+  gg.toast("BYPASS berjalan")
+  gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+  gg.searchNumber("5001;1.1;1F::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults(850)
+  gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+  gg.searchNumber("5001;1.1;1F::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults(850)
+  gg.alert("BYPASS 100% diaktifkan")
+end
+function M1()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("4.814603e21;3.5032462e-44;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("-5.5693206e-40;4.814603e21;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("5.1848043e-44;-1.0285578e-38;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("304.00009155273;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.toast("WallHack Global telah aktif")
+end
+function M2()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("8196;256;8204;256;8200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("8200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("391", gg.TYPE_DWORD)
+  gg.toast("Body Red telah aktif")
+end
+function M3()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("32,769;-2,134,900,722", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("32769", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("32777", gg.TYPE_DWORD)
+  gg.toast("Body Green telah aktif")
+ end
+function M4()
+  gg.clearResults()
+  gg.searchNumber('999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber('10.90734863281;0.00999999978', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber('10.9073486328', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(300)
+  gg.editAll('10.411', gg.TYPE_FLOAT)
+  gg.toast('SpeedHack telah aktif')
+end
+function M6()
+  gg.clearResults()
+  gg.searchNumber("1868784978;1850305641;28518", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1868784978", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("1868756421", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("100000", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("No Recoil telah aktif")
+end
+function M7()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.searchNumber("8.0F;1.20000004768F;0.80000001192F;1.5F;0.80000001192F;1.5F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("8", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("0", gg.TYPE_FLOAT)
+  gg.toast("No Grass telah aktif")
+end
+function M8()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.clearResults()
+  gg.searchNumber("100F;1F;1,008,981,770D:99", gg.TYPE_FLOAT, false, gg.SING_EQUAL, 0, -1)
+  gg.searchNumber("100", gg.TYPE_FLOAT, false, gg.SING_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("-99", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.toast("Black Sky telah aktif")
+end
+function M9()
+  gg.clearResults()
+  gg.searchNumber("0.1;1000;88;60;30", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("30", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("-999", gg.TYPE_FLOAT)
+  gg.toast("Visual Lift aktif")
+end
+function M15()
+  gg.clearResults()
+  gg.searchNumber("7.13142681122;0.53447723389;22.6400718689", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("22.6400718689", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("9621", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Antenna Run aktif")
+end
+function M10()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.searchNumber("0.53446006775F;-1.68741035461F:501", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("-1.68741035461", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1995)
+  gg.editAll("19995", gg.TYPE_FLOAT)
+  gg.toast("Antenna 50%")
+  gg.clearResults()
+  gg.searchNumber("18.38612365723F;0.54026412964F:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("18.38612365723F;0.54026412964F:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1995)
+  gg.editAll("19995", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Antenna telah aktif ")
+end
+function M5()
+  gg.searchNumber("999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.clearResults()
+  gg.searchNumber("3.5;1;200;20::999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("3.5;1;200;20", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(300)
+  gg.editAll("-1.0e10", gg.TYPE_FLOAT)
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.clearResults()
+  gg.searchNumber("8;16;18", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("8;16;18", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("38.5", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("AimBot telah aktif")
+end
+function M11()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1,092,081,726;1,003,658,240;923,795,456", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1,092,081,726", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(50)
+  gg.editAll("1,135,081,726", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("High view telah aktif")
+end
+function M12()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1,135,081,726;1,003,658,240;923,795,456", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1,135,081,726", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(50)
+  gg.editAll("1,092,081,726", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("High view dimatikan")
+end
+function M13()
+ gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1F;-8.6457681e12F;15F;28F;16F;26F;8F;18F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("15.0F;28.0F;16.0F;26.0F;8.0F;18.0F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(500)
+  gg.editAll("75", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("NEW Magic Bullet aktif")
+end
+function M14()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("0.05499718338;1.0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("9999", gg.TYPE_FLOAT)
+gg.toast("Body White has been active")
+end
+function MDN()
+ DN = gg.choice({
+    "\226\156\137\239\184\143 Telegram: @OrangBias4",
+    "💵 https://www.paypal.me/JPutra722",
+    "\226\158\168\239\184\143 Back \226\158\168\239\184\143"
+  }, nil, "Thanks For Donating")
+  if DN == 1 then
+    JSP()
+  end
+  if DN == 2 then
+    JSP2()
+  end
+  if DN == 3 then
+    HOME()
+  end
+  HOMEDM = -1
+end
+function JSP()
+gg.alert("https://t.me/OrangBias4")
+end
+function JSP2()
+gg.alert("https://www.paypal.me/JPutra722")
+end
+ function MC()
+  KR = gg.choice({
+    "\226\156\137\239\184\143 Telegram: @OrangBias4",
+    "\226\156\137\239\184\143 Instagram: @arsyah_g",
+    "\226\158\168\239\184\143 Back \226\158\168\239\184\143"
+  }, nil, "Untuk mendapatkan Script VIP Hubungi saya(Arsyah G)")
+  if KR == 1 then
+    MC()
+  end
+  if KR == 2 then
+    MC()
+  end
+  if KR == 3 then
+    HOME()
+  end
+  HOMEDM = -1
+end
+function EXIT()
+  print(os.date("Free Script PUBG MOBILE"))
+  print("\226\156\148\239\184\143Youtube Channel: Arsyah G")
+  print("\226\156\148\239\184\143Telegram: @OrangBias4")
+  print("\226\156\148\239\184\143Instagram: @arsyah_g")
+  print("Contact Saya jika ada masalah terhadap Script")
+  os.exit()
+end
+while true do
+  if gg.isVisible(true) then
+    HOMEDM = 1
+    gg.setVisible(false)
+  end
+  if HOMEDM == 1 then
+    HOME()
+  end
+end
